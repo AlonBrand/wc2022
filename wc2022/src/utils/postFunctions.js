1fb6b17126491d9ec63406197f93df57aa6c18a6
Alon Brand <alonbrand01@gmail.com>
2022-11-04 14:08:49 +0200

    deply changes

--- wc2022/src/utils/postFunctions.js
+++ wc2022/src/utils/postFunctions.js
@@ -5,9 +5,10 @@ export const postSignUp = async ({ name, password, updateConnectedUserName, setI
         body: JSON.stringify({ name: name, password: password }),
     };
     try {
-        let response = await fetch("http://127.0.0.1:5000/sign-up", requestOptions);
+        let response = await fetch("https://alon-wc22.herokuapp.com/", requestOptions);
         response.json()
             .then((data) => {
+                console.log(data)
                 updateConnectedUserName(`Hi, ${data?.user_name}`)
                 setIsConnect(()=>true);
             });

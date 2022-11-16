7c972e21c8a03f8d52e5b982de88fdb414f70616
Alon Brand <alonbrand01@gmail.com>
2022-11-16 15:56:58 +0200

    fix games UI

--- wc2022/src/components/GameTab.jsx
+++ wc2022/src/components/GameTab.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./GameTab.css";
+import { flagsPaths } from "../constants/games";
+import ReactCountryFlag from "react-country-flag"
 
 export const GameTab = ({ id, teamA, teamB, date, info }) => {
     const [scoreA, setScoreA] = useState();
@@ -28,30 +30,94 @@ export const GameTab = ({ id, teamA, teamB, date, info }) => {
         if(scoreA === undefined || scoreB === undefined || scoreA < 0 || scoreB < 0) return true;
         return false;
     }
+    const getFlagIcon = () => {
+        return  (
+            <>
+                <div 
+                    style={{
+                        "display":"flex",
+                        "flexDirection":"row",
+                        "justifyContent":"space-around",
+                        "paddingTop": "15px"
+                    }}
+                >
+                    <div>
+                        <ReactCountryFlag
+                            countryCode={flagsPaths[teamA]}
+                            svg
+                            style={{
+                                width: '2em',
+                                height: '2em',
+                            }}
+                            title={teamA}
+                        />
+                        <h4 style={{"paddingTop": "5px"}}>{teamA}</h4>
+                    </div>
+                        <h3 style={{"paddingTop":"15px","text-align":"center"}}>VS</h3>
+                    
+                    <div>
+                    <ReactCountryFlag
+                            countryCode={flagsPaths[teamB]}
+                            svg
+                            style={{
+                                width: '2em',
+                                height: '2em',
+                            }}
+                            title={teamB}
+                        />
+                        <h4 style={{"paddingTop": "5px"}}>{teamB}</h4>
+                    </div>
+                </div>
+            </>
+        )
+
+    }
+    const getDateTime = () => {
+        return  (
+            <>
+               <div 
+                    style={{
+                        "display":"flex",
+                        "flexDirection":"row",
+                        "justifyContent":"space-evenly",
+                        "paddingTop": "20px"
+                    }}
+                >
+                    <div>
+                        {date?.toLocaleDateString("he-IL")}
+                    </div>
+                    <div>
+                        {date?.toLocaleTimeString("he-IL")}
+                    </div>
+                </div>
+            </>
+        )
+
+    }
 
     return (
-        <div className="game-tab-container" >
-            <h2>{teamA} X {teamB}</h2>
-            <br></br>
-            {date?.toString()}
-            <br></br>
-            {
-                info !== undefined && 
-                <br></br> &&
-                <p>{info}</p>
-
-            }
-            <br></br>
-            <form onSubmit={(e) => {e.preventDefault(); betOnGame()}}>
-                <div className="bet-line">
-                    <input id="left-bet" type="number" placeholder={teamA} onChange={(e)=>setScoreA(e.target.value)}></input>
-                    <input id="right-bet" type="number" placeholder={teamB} onChange={(e)=>setScoreB(e.target.value)}></input>
-                </div>
+        <div className="game-tab-container" style={{marginBottom: "30px"}}>
+                {getFlagIcon()}
                 <br></br>
-                <input id="bet-button" type="submit" value={'Bet'} disabled={validateInput()}></input>
-            </form>
-            {!isAvailableGame && <h3 style={{"paddingTop": "10px"}}>Game is not available!</h3>}
-            <div id={"response-placeholder"} style={{"paddingTop": "10px"}}></div>
+                {getDateTime()}
+                <br></br>
+                {
+                    info !== undefined && 
+                    <br></br> &&
+                    <p>{info}</p>
+
+                }
+                <br></br>
+                <form onSubmit={(e) => {e.preventDefault(); betOnGame()}}>
+                    <div className="bet-line">
+                        <input id="left-bet" style={{height: "30px", textAlign: "center"}} type="number" placeholder={teamA} onChange={(e)=>setScoreA(e.target.value)}></input>
+                        <input id="right-bet" style={{height: "30px", textAlign: "center"}} type="number" placeholder={teamB} onChange={(e)=>setScoreB(e.target.value)}></input>
+                    </div>
+                    <br></br>
+                    <input id="bet-button" className="bet-button" type="submit" value={'Bet'} disabled={validateInput()}></input>
+                </form>
+                {!isAvailableGame && <h3 style={{padding: "10px", marginBottom: "20px"}}>Game is not available!</h3>}
+                <div id={"response-placeholder"} style={{"paddingTop": "10px"}}></div>
         </div>
     )
 }
